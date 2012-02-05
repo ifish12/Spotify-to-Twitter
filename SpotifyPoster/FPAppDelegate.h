06d577acc0e4fb6d3d5c99879164cf44eee2a1d4
Geoff Shapiro <iFish@Geoff-Shapiros-MacBook-Pro.local>
2012-02-04 19:46:24 -0500

    Initial commit

--- SpotifyPoster/FPAppDelegate.h
+++ SpotifyPoster/FPAppDelegate.h
@@ -12,4 +12,6 @@
 
 @property (assign) IBOutlet NSWindow *window;
 
+- (IBAction)pasteFromSpotifyToTwitter:(id)sender;
+
 @end
--- SpotifyPoster/FPAppDelegate.m
+++ SpotifyPoster/FPAppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "FPAppDelegate.h"
+#import "Spotify.h"
 
 @implementation FPAppDelegate
 
@@ -15,6 +16,26 @@
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
+}
+
+- (IBAction)pasteFromSpotifyToTwitter:(id)sender {
+    SpotifyApplication *app = [SBApplication applicationWithBundleIdentifier:@"com.spotify.client"];
+    if (app) {
+        SpotifyTrack *track = [app currentTrack];
+        NSString *urlString = [NSString stringWithFormat:@"Now Playing: %@ - %@ - %@",
+                               track.name ? track.name : @"", track.artist, track.album];
+        NSString *encodedTrackName = (__bridge_transfer NSString *)CFURLCreateStringByAddingPercentEscapes(kCFAllocatorDefault,
+                                                                                         (__bridge CFStringRef)urlString,
+                                                                                         NULL,
+                                                                                         CFSTR("!*'();:@&=+$,/?%#[]"),
+                                                                                         kCFStringEncodingUTF8);
+        NSString *fullUrlString = [NSString stringWithFormat:@"Twitter://post?message=%@", encodedTrackName];
+        NSURL *finalUrl = [NSURL URLWithString:fullUrlString];
+        [[NSWorkspace sharedWorkspace] openURL:finalUrl];
+//        NSLog(@"%@", [app currentTrack].name);
+    } else  {
+        NSLog(@"empty");
+    }
 }
 
 @end
